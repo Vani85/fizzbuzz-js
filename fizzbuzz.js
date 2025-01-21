eb8350d979b7b6d307c1e56d66d7f3556c69580e
Kalaivani Natarajan <kalaivani.natarajan@softwire.com>
2025-01-21 10:33:45 +0000

    Stretch goals

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,21 +1,48 @@
 
+let message = "";
+
+function append(number) {   
+
+        if(number%3 == 0)
+            message += "Fizz";
+
+        if(number%5 == 0)
+            message += "Buzz";
+        
+}
 
 // Here, we create our main function.
-function fizzbuzz() {
-    
+function fizzbuzz() {    
     console.log('Welcome to FizzBuzz!');
-
+    
     // Print numbers 1 - 100
-    for(let i = 1; i<=100; i++) {
-        // Number divisible by 3 and 5
-        if(i%3 == 0 && i%5==0)
-            console.log("FizzBuzz");
-        // Number divisible by 3
-        else if(i%3 == 0) 
-            console.log("Fizz");
-        //Number divisible by 5
-        else if(i%5 == 0)
-            console.log("Buzz");        
+    for(let i = 1; i<=195; i++) {
+        let appended = false;
+        message = "";
+
+        if(i%11 == 0) {
+            message+="Bong";
+            appended = true;
+        }
+
+        if(i%7 == 0) {
+            message += "Bang";
+            append(i);
+            appended = true;
+        }
+
+        if(i%13 ==0) {
+            message = "Fezz" + message;
+            append(i);
+            appended = true;
+        }
+
+        if(!appended)
+            append(i);
+        
+       
+        if(message != "")
+            console.log(message);
         else
             console.log(i);
     }    
